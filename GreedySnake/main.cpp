6e2e69e97d7e9485d5f23b8ef49cd87999926808
hj <hujunchn@163.com>
2020-09-01 09:33:50 +0800

    update

--- GreedySnake/main.cpp
+++ GreedySnake/main.cpp
@@ -1,7 +1,18 @@
+#include <windows.h>
 #include "controller.h"
+
+void HideCursor()
+{
+	CONSOLE_CURSOR_INFO cursor;
+	cursor.bVisible = FALSE;
+	cursor.dwSize = sizeof(cursor);
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleCursorInfo(handle, &cursor);
+}
 
 int main()//程序入口
 {
+	HideCursor();//隐藏光标
     Controller c;//声明一个Controller类
     c.Game();//整个游戏循环
     return 0;
